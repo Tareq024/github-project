9c8c2c842366cbfa1cf3f2535f6bc3d46e844629
Tareq Fasihuddin <tareq024@gmail.com>
2022-10-14 19:30:03 +0200

    add github projecct 1

---
+++ new-code-calss-10--main/github project 1.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+class Firefox():
+    def firefox(self):
+        driver=webdriver.Firefox(executable_path="D:\\softwarem testing class\\Class 10\\tools\\geckodriver.exe")
+        driver.get("https://www.iiuc.ac.bd")
+
+test_object=Firefox()
